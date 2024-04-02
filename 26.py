57cbba5f356354c202b308d296f6e4cbedefd6ff
Dynamo285K <113776601+Dynamo285K@users.noreply.github.com>
2024-04-02 22:40:37 +0200

    Update 26.py

--- 26.py
+++ 26.py
@@ -2,8 +2,8 @@ import tkinter as tk
 import random
 win = tk.Tk()
 
-HEIGHT = 250
-WIDTH = 250
+HEIGHT = 500
+WIDTH = 500
 
 canvas = tk.Canvas(height=HEIGHT,width=WIDTH,bg= 'skyblue')
 canvas.pack()
@@ -15,36 +15,33 @@ def draw():
     random_x = random.randint(0,WIDTH)
     random_y = random.randint(0,HEIGHT)
     print(random_x,random_y)
+    list_of_points = [[0,HEIGHT]]
     if udolie_ci_vrch == 1:
-        temp = random.randint(random_y,HEIGHT)
-        list_of_points = [[0,HEIGHT]]
+        temp = random.randint(200,400)
         for i in range(0,random_x,10):
-            point_y = random.randint(temp-10,temp)
+            point_y = random.randint(temp-5,temp)
             list_of_points.append([i,point_y])
             temp = point_y
-        print(list_of_points)
-        temp1 = random_y
-        list_of_points.append([random_x,random_y])
+        peak = list_of_points[-1]
+        temp1 = random.randint(peak[1],peak[1]+5)
         for i in range(random_x,WIDTH+1,10):
-            point_y = random.randint(temp1, min(temp1 + 10, HEIGHT))
+            point_y = random.randint(temp1,min(temp1 + 5, HEIGHT))
             list_of_points.append([i,point_y])
             temp1 = point_y
         list_of_points.append([WIDTH,point_y])
         list_of_points.append([WIDTH,HEIGHT])
         print(list_of_points)
-        canvas.create_polygon(list_of_points,outline='black', fill=rand_color)
+        canvas.create_polygon(list_of_points, outline='black', fill=rand_color)
     else:
-        list_of_points = [[0,HEIGHT]]
-        temp = random.randint(0,random_y)
+        temp = random.randint(200,300)
         for i in range(0,random_x,10):
-            point_y = random.randint(temp, min(temp + 10, random_y))
+            point_y = random.randint(temp,temp+5)
             list_of_points.append([i,point_y])
             temp = point_y
-        print(list_of_points)
-        temp1 = random_y
-        list_of_points.append([random_x,random_y])
+        peak = list_of_points[-1]
+        temp1 = random.randint(peak[1] - 5,peak[1])
         for i in range(random_x,WIDTH+1,10):
-            point_y = random.randint(max(temp1 - 10, 0),temp1)
+            point_y = random.randint( temp1 - 5, temp1)
             list_of_points.append([i,point_y])
             temp1 = point_y
         list_of_points.append([WIDTH,point_y])
@@ -52,10 +49,7 @@ def draw():
         print(list_of_points)
         canvas.create_polygon(list_of_points, outline='black', fill=rand_color)
 
-
 win.bind("<space>", lambda event: draw())  # Bind space key to generate a new landscape
 
-
 draw()
-
 win.mainloop()
